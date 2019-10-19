2ce73dd151271a9f971b06be05a27b0f554da9a6
Jacob Medel <jmedel@uh.edu>
2019-10-19 00:54:22 -0500

    Buttons layed out for coding

---
+++ client/pet_interface/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Choices from './ChoicePage.js';
+
+function App() {
+    return (
+        <Choices/>
+    );
+}
+export default App;
